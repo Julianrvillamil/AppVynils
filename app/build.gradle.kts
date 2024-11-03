e133a495e92d1a06bf21fa947f81ef4d3d1ad87e
SergioMena <s.menaz@uniandes.edu.co>
2024-11-02 22:17:27 -0600

    Se sube prueba instrumentada para listado de albumes.
    Se sube prueba instrumentada para listado de albumes.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.espresso.contrib)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
