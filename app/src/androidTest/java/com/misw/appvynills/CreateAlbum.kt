ab486d63109b23176a05bd431a463a4f48376ae5
Leonel Fonseca <n.fonsecao@uniandes.edu.co>
2024-11-30 19:41:27 -0500

    prueba crear album

---
+++ app/src/androidTest/java/com/misw/appvynills/CreateAlbum.kt
@@ -0,0 +1,145 @@
+package com.misw.appvynills
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withClassName
+import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class CreateAlbum {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(SplashActivity::class.java)
+
+    @Test
+    fun createAlbum() {
+        val floatingActionButton = onView(
+            allOf(
+                withId(R.id.fabCreateAlbum), withContentDescription("crear nuevo album"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.nav_host_fragment_activity_main),
+                        0
+                    ),
+                    3
+                ),
+                isDisplayed()
+            )
+        )
+        floatingActionButton.perform(click())
+
+        val appCompatEditText = onView(
+            allOf(
+                withId(R.id.inputName),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.cardViewForm),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText.perform(replaceText("ok"), closeSoftKeyboard())
+
+        val appCompatEditText2 = onView(
+            allOf(
+                withId(R.id.inputCover),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.cardViewForm),
+                        0
+                    ),
+                    3
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText2.perform(replaceText("ok.com"), closeSoftKeyboard())
+
+        val appCompatEditText3 = onView(
+            allOf(
+                withId(R.id.inputReleaseDate),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.cardViewForm),
+                        0
+                    ),
+                    5
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText3.perform(replaceText("2024-11-30"), closeSoftKeyboard())
+
+        val appCompatEditText4 = onView(
+            allOf(
+                withId(R.id.inputDescription),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.cardViewForm),
+                        0
+                    ),
+                    7
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText4.perform(replaceText("ok"), closeSoftKeyboard())
+
+        val materialButton = onView(
+            allOf(
+                withId(R.id.buttonSubmit), withText("Crear"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                        1
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton.perform(click())
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
